d3231c9c6bf557a240fbaafb164ca9e10d5ba902
liuzhiyi1992 <55137840@qq.com>
2015-12-16 18:06:56 +0800

    Initial Commit

---
+++ FlagProgressBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlagProgressBar
+//
+//  Created by lzy on 15/12/16.
+//  Copyright © 2015年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlagProgressBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlagProgressBar
+//
+//  Created by lzy on 15/12/16.
+//  Copyright © 2015年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
